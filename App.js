b44e4d09341b2cb19eaeec46a13abea9f43e66d9
bryan aviles <easyrun32@gmail.com>
2021-04-04 11:45:37 -0400

    deploy to expo

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import React, { useState } from "react";
-// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-// import LocationAuthorization from "./components/LocationAuthorization";
-// import Map from "./components/Map";
 
 export default function App() {
-  // const [ displayMap, setDisplayMap ] = useState(true);
-
   return (
     <View style={styles.container}>
-      {/* <StatusBar style="auto" /> */}
-      {/* {displayMap ? <Map/> : <Text>Can't work without authorizaion</Text>} */}
-      <TouchableOpacity
-      // style={styles.button} onPress={() => startApp()}
-      >
-        <Text>START APP</Text>
+      <TouchableOpacity>
+        <Text>hello world</Text>
       </TouchableOpacity>
     </View>
   );
-
-  // function startApp() {
-  //   LocationAuthorization()
-  //   .then(permission => setDisplayMap(permission))
-  //   .catch(() => console.log("can't start without location permissions, activate manually"))
-  // };
 }
 
 const styles = StyleSheet.create({
