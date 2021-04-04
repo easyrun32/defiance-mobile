700f7c1d1890dd22a113d16a76f03c60b5d3e3f3
bryan aviles <easyrun32@gmail.com>
2021-04-04 10:33:04 -0400

    making it work

--- App.js
+++ App.js
@@ -1,39 +1,40 @@
-import React , { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import LocationAuthorization from "./components/LocationAuthorization";
-import Map from "./components/Map";
+import React, { useState } from "react";
+// import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+// import LocationAuthorization from "./components/LocationAuthorization";
+// import Map from "./components/Map";
 
 export default function App() {
-
-  const [ displayMap, setDisplayMap ] = useState(true);
+  // const [ displayMap, setDisplayMap ] = useState(true);
 
   return (
     <View style={styles.container}>
       {/* <StatusBar style="auto" /> */}
-      {displayMap ? <Map/> : <Text>Can't work without authorizaion</Text>}
-      <TouchableOpacity style={styles.button} onPress={()=>startApp()}>
+      {/* {displayMap ? <Map/> : <Text>Can't work without authorizaion</Text>} */}
+      <TouchableOpacity
+      // style={styles.button} onPress={() => startApp()}
+      >
         <Text>START APP</Text>
       </TouchableOpacity>
     </View>
   );
 
-  function startApp() {
-    LocationAuthorization()
-    .then(permission => setDisplayMap(permission))
-    .catch(() => console.log("can't start without location permissions, activate manually"))
-  };
-};
+  // function startApp() {
+  //   LocationAuthorization()
+  //   .then(permission => setDisplayMap(permission))
+  //   .catch(() => console.log("can't start without location permissions, activate manually"))
+  // };
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  button:{
+  button: {
     position: "absolute",
-    bottom: 60
-  }
+    bottom: 60,
+  },
 });
