898d9b078da3c57f3a5e4089c3306cad312e15e5
bryan aviles <easyrun32@gmail.com>
2021-04-06 03:08:50 -0400

    hello world

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  View,
+  Alert,
+} from "react-native";
 import io from "socket.io-client";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
-  const [text, onChangeText] = useState("Useless Text");
+  const [text, onChangeText] = useState("hello world");
 
   useEffect(() => {
     const socketClient = io("https://deliverytracker.herokuapp.com/");
@@ -27,22 +36,46 @@ export default function App() {
           value={text}
         />
       </SafeAreaView>
-      <TouchableOpacity>
-        <Text>hello world</Text>
+      <TouchableOpacity
+        style={styles.loginScreenButton}
+        onPress={() => Alert.alert(`${text}`)}
+        underlayColor="#fff"
+      >
+        <Text style={styles.loginText}>Login</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    width: 100,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    textAlign: "center",
+  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  button: {
-    position: "absolute",
-    bottom: 60,
+  loginScreenButton: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    backgroundColor: "#1E6738",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff",
+  },
+  loginText: {
+    color: "#fff",
+    textAlign: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
   },
 });
