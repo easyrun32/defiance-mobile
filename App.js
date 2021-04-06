6f495e8d0aac88739b729f1a2b454d46d455717a
bryan aviles <easyrun32@gmail.com>
2021-04-06 02:28:26 -0400

    input testing

--- App.js
+++ App.js
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [text, onChangeText] = useState("Useless Text");
+
+  useEffect(() => {
+    const socketClient = io("https://deliverytracker.herokuapp.com/");
+    socketClient.on("connect", () => {
+      socketClient.emit("new-user", {
+        id: 4545,
+        store: "psq2",
+        role: "driver",
+      });
+    });
+    return () => {
+      socketClient.close();
+    };
+  });
   return (
     <View style={styles.container}>
+      <SafeAreaView>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />
+      </SafeAreaView>
       <TouchableOpacity>
         <Text>hello world</Text>
       </TouchableOpacity>
